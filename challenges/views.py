8405a1a85a106ba9cafc6888755860d2725471ba
Santosh Narayanan <santosh33@gmail.com>
2024-04-26 17:13:51 +0200

    Path convertor

--- challenges/views.py
+++ challenges/views.py
@@ -1,16 +1,10 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Eat no meat fro entire month January")
-
-
-def indexfeb(request):
-    return HttpResponse("febraury  challenges are coming up")
-
+def monthly_challenge_number(request, month):
+    return HttpResponse(month)
 
 def monthly_challenge(request, month):
     if month == 'january':
